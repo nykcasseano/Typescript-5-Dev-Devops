da9fa7ab9da13ddc8763aab64ab32691fc9d061c
nykcasseano <nykcasseano13@gmail.com>
2023-09-17 15:41:40 -0300

    adding more files to the project

---
+++ section3/3.1PrimitiveNarrowing.ts
@@ -0,0 +1,14 @@
+//Narrow Down primitives: with typeof
+
+function getNumberValue(arg: unknown): number{
+    if (typeof arg === 'number'){
+        return arg;
+    }
+    if (typeof arg === 'string'){
+        return Number(arg);
+    }
+    throw new Error(`Unsupported format: ${JSON.stringify(arg)}`);
+}
+
+console.log(getNumberValue('dfg'));
+getNumberValue({})
